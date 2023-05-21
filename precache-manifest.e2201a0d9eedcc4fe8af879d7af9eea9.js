9673b494ae2991c4b2c4cd4b26e04249ab59720f
Mrabdulbtw <abdulofficialbtw@gmail.com>
2023-05-21 13:09:07 +0530

    Updates

---
+++ precache-manifest.e2201a0d9eedcc4fe8af879d7af9eea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c9c36fa82208b87be44",
+    "url": "/cart/static/css/main.bc38199a.chunk.css"
+  },
+  {
+    "revision": "3c9c36fa82208b87be44",
+    "url": "/cart/static/js/main.3c9c36fa.chunk.js"
+  },
+  {
+    "revision": "717fa839111c19b454fb",
+    "url": "/cart/static/js/1.717fa839.chunk.js"
+  },
+  {
+    "revision": "2533cd88b3fcc13cbef1",
+    "url": "/cart/static/js/runtime~main.2533cd88.js"
+  },
+  {
+    "revision": "f05efc9a059cfc9384e6c3498fdea877",
+    "url": "/cart/index.html"
+  }
+];
